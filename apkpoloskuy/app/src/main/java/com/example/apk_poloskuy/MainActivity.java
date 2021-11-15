9f12a3101c1d79224d68fffc9ad9013a549e66c3
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2021-11-15 07:55:23 +0700

    membuat link login to register dan sebaliknya

--- apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/MainActivity.java
+++ apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/MainActivity.java
@@ -6,17 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 private Button btnMove;
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
     btnMove = (Button) findViewById(R.id.btnlog);
+    /*deklarasi link ke daftar*/
+        TextView textView = findViewById(R.id.tvToreg);
 
-    btnMove.setOnClickListener(new View.OnClickListener() {
+    textView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
         Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
--- apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/RegisterActivity.java
+++ apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.example.apk_poloskuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        /*deklarasi teksview untuk berpindah ke  login*/
+        textView = (TextView) findViewById(R.id.tvTolog);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
     }
 }
