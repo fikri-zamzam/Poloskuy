8757abb1b34ff008dced68591ea7f07ddc13c95b
unknown <muhamadkbar47@gmail.com>
2021-11-21 20:16:29 +0700

    Akbar_Checkout

---
+++ checkoutpoloskuy/app/src/main/java/com/example/checkout_poloskuy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.checkout_poloskuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
