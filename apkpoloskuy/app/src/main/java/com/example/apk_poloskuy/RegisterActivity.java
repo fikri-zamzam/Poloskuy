1aff3d026391dfdaba75aacf2425ce41f70dffaa
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2021-11-22 09:03:09 +0700

    membuat fragment tampilan dashboard

--- apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/RegisterActivity.java
+++ apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/RegisterActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
     TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,22 @@ public class RegisterActivity extends AppCompatActivity {
         /*deklarasi teksview untuk berpindah ke  login*/
         textView = (TextView) findViewById(R.id.tvTolog);
 
+        /*deklarasi button*/
+        button = (Button)findViewById(R.id.btnreg);
+
+
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class );
+                startActivity(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this,DashboarActivity.class );
                 startActivity(intent);
             }
         });
