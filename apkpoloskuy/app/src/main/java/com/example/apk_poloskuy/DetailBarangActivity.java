1f0f5a87618c3e6d947478363dbd1c17d0034f6f
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2022-01-06 10:20:38 +0700

    update tampilan recyclerview dan detail barang, menambah intent ke whatsapp

--- apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/DetailBarangActivity.java
+++ apkpoloskuy/app/src/main/java/com/example/apk_poloskuy/DetailBarangActivity.java
@@ -3,9 +3,13 @@ package com.example.apk_poloskuy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.apk_poloskuy.Model.ModelBarang;
@@ -20,6 +24,7 @@ public class DetailBarangActivity extends AppCompatActivity {
         TextView nama,harga,desc;
         ImageView gambar;
 
+        Button btnWa;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_barang);
 
@@ -42,6 +47,27 @@ public class DetailBarangActivity extends AppCompatActivity {
                 .centerCrop()
                 .placeholder(R.drawable.ic_launcher_foreground)
                 .into(gambar);
+
+
+        //BUtton chat whastapp
+
+        btnWa = findViewById(R.id.buttonChat);
+        btnWa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link= null;
+                try {
+                    link = "https://api.whatsapp.com/send?phone=+6281249345670"+"&text=Hallo.\n" +
+                            "Saya ingin bertanya ketersediaan item di POLOSKUY!";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "Gagal membuka WhatsApp", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     private String Rupiah(Double number){
